d6741df0398275f07a038a5ad45b4edbee6c8d02
Tom Gehder <Gehda@Toms-MacBook-Pro.local>
2016-07-03 23:52:08 +0200

    Tab adjustent

--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -1,25 +1,20 @@
 import {Component} from '@angular/core'
 import {HomePage} from '../home/home';
-import {AboutPage} from '../about/about';
-import {ContactPage} from '../contact/contact';
 import { FotosPage } from '../fotos/fotos';
+
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
 })
 export class TabsPage {
 
-  private tab1Root: any;
-  private tab2Root: any;
-  private tab3Root: any;
-  private tab4Root: any;
+  private tabHome: any;
+  private tabFotos: any;
 
   constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
-    this.tab1Root = HomePage;
-    this.tab2Root = AboutPage;
-    this.tab3Root = ContactPage;
-    this.tab4Root = FotosPage;
+    this.tabHome = HomePage;
+    this.tabFotos = FotosPage;
   }
 }
