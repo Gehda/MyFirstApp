11c08230d69a2d3ec493a75eb36b0d82402dcbfd
Tom Gehder <Gehda@Toms-MacBook-Pro.local>
2016-07-06 11:24:36 +0200

    Added costum icons

--- app/pages/fotos/fotos.ts
+++ app/pages/fotos/fotos.ts
@@ -15,16 +15,20 @@ export class FotosPage implements OnInit {
      public myArray = [
          {  objectId:1,
             name:'hello',
-            description:'it is this element'
+            description:'it is this element',
+            icon:'laptop'
         },{  objectId:2,
             name:'hello123',
-            description:'it is this element'
+            description:'it is this element',
+            icon:'book'
         },{  objectId:3,
             name:'hello321',
-            description:'it is this element'
+            description:'it is this element',
+            icon:'american-football'
         },{  objectId:4,
             name:'hell123123o',
-            description:'it is this element'
+            description:'it is this element',
+            icon:'leaf'
         },
      ]
     ngOnInit() { }
